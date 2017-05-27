f5ed297827eb99e47841b9fea4e310d35603025d
Solo <solohan22@gmail.com>
2017-05-27 01:33:12 -0500

    Kalman filter implemetation

--- src/kalman_filter.cpp
+++ src/kalman_filter.cpp
@@ -19,21 +19,61 @@ void KalmanFilter::Init(VectorXd &x_in, MatrixXd &P_in, MatrixXd &F_in,
 
 void KalmanFilter::Predict() {
   /**
-  TODO:
     * predict the state
   */
+  x_ = F_ * x_;
+  MatrixXd Ft = F_.transpose();
+  P_ = F_ * P_ * Ft + Q_;
 }
 
 void KalmanFilter::Update(const VectorXd &z) {
   /**
-  TODO:
-    * update the state by using Kalman Filter equations
+    * state update using Kalman Filter 
   */
+  VectorXd z_pred = H_ * x_;
+  VectorXd y = z - z_pred;
+  MatrixXd Ht = H_.transpose();
+  MatrixXd S = H_ * P_ * Ht + R_;
+  MatrixXd Si = S.inverse();
+  MatrixXd PHt = P_ * Ht;
+  MatrixXd K = PHt * Si;
+
+  //new estimate
+  x_ = x_ + (K * y);
+  long x_size = x_.size();
+  MatrixXd I = MatrixXd::Identity(x_size, x_size);
+  P_ = (I - K * H_) * P_;
 }
 
 void KalmanFilter::UpdateEKF(const VectorXd &z) {
   /**
-  TODO:
-    * update the state by using Extended Kalman Filter equations
+    * state update using Extended Kalman Filter 
   */
+  double rho = sqrt( pow(x_[0],2) + pow(x_[1],2) );
+  double phi;
+  double rho_dot;
+
+  if (fabs(rho < 0.0001)) { //if range equals 0, set both range and bearning to be 0
+    phi = 0;
+    rho_dot = 0;
+  } else { 
+    phi = atan(x_[1] / x_[0]);
+    rho_dot = ((x_[0] * x_[2] + x_[1] * x_[3]) / rho);
+  }
+
+  MatrixXd z_pred(3, 1);
+  z_pred << rho, phi, rho_dot;  // use H(x) directly as predictioin
+
+  MatrixXd Ht = H_.transpose();
+  MatrixXd S = H_ * P_ * Ht + R_;
+  MatrixXd Si = S.inverse();
+  MatrixXd PHt = P_ * Ht;
+  MatrixXd K = PHt * Si;
+  
+  //new estimate
+  VectorXd y = z - z_pred;
+  x_ = x_ + (K * y);
+  long x_size = x_.size();
+  MatrixXd I = MatrixXd::Identity(x_size, x_size);
+  P_ = (I - K * H_) * P_;
 }
